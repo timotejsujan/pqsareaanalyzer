207abe301dd2ac358cffa162e43c89d46e13612c
Timotej Šujan <tim.sujan@gmail.com>
2020-07-08 01:18:02 +0200

    new version

---
+++ target/resources/g4.R
@@ -0,0 +1,19 @@
+library(BiocManager)
+library(pqsfinder)
+
+options(echo = TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+seq <- readDNAStringSet(args[1], "fasta")
+file.create(args[2]);
+for (i in 1:length(seq)) {
+  # following lines cannot be changed
+  # id = 3A892
+  pqs <- pqsfinder(seq[[i]], strand = "*", overlapping = FALSE, min_score = 52)
+  # --------------------------------
+  if (length(pqs@elementMetadata@listData[["score"]]) > 0) {
+    cat(paste0(";segment number: ", i), file = args[2], sep = "\n", append = TRUE)
+    dss <- as(pqs, "DNAStringSet")
+    writeXStringSet(dss, file = args[2], format = "fasta", append = TRUE)
+  }
+}
